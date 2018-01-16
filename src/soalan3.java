38578d8e7f9cd24efa2077132a4a6c9432c6ae9e
user <user@DESKTOP-OAL422G>
2018-01-17 01:20:55 +0800

    Java5

---
+++ src/soalan3.java
@@ -0,0 +1,42 @@
+/*Anda dikehendaki membuat satu aturcara yang memberi 
+ *maklumat status penderma berdasarkan input 
+ *jenis darah yang diberikan oleh pengguna
+ *Gunakan struktur kawalan if sahaja dalam membuat
+ *syarat jenis darah. Berikut adalah maklumat
+ *mengenai penderma berdasarkan jenis kumpulan darah
+
+ */
+
+/**
+ *
+ * @author user
+ */import java.util.Scanner;
+public class soalan3 {
+    public static void main(String[] args){
+    Scanner taip = new Scanner(System.in);
+    
+        String nama;
+        char kumpdarah;
+        String makluman;
+        System.out.println("Nama anda");
+        nama = taip.nextLine();
+        
+       System.out.println("kumpulan darah : Taip A/B/O/Z[AB] ");
+       kumpdarah = taip.next().charAt(0);
+       taip.close();
+       
+       if (kumpdarah=='A'){
+       } makluman="A dan AB sahaja";
+       if(kumpdarah=='B'){
+       } makluman="B dan AB sahaja";
+       if (kumpdarah=='Z'){
+       } makluman="AB sahaja";
+       if (kumpdarah=='O'){
+        makluman="A,B,AB dan O";
+       }
+       
+       System.out.println("Nama :"+nama);
+       System.out.println("Kumpulan darah :" +kumpdarah);
+       System.out.println("Anda boleh menderma kepada " +makluman);
+    }
+}
